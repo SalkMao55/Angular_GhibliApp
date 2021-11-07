50e4171534a4a47c31490f0e74727edec807a2e9
Salomon <13030564@itcelaya.edu.mx>
2021-11-07 17:09:15 -0600

    Routing Module Implemented

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+// Modules Rounting
+import { Routes, RouterModule } from "@angular/router";
+// Components to route
+import { HomeComponent } from './pages/home/home.component';
+import { FilmsComponent } from './pages/films/films.component';
+import { LocationsComponent } from './pages/locations/locations.component';
+import { PeopleComponent } from './pages/people/people.component';
+import { SpeciesComponent } from './pages/species/species.component';
+import { VehiclesComponent } from './pages/vehicles/vehicles.component';
+
+// Array of Routes
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'films', component: FilmsComponent },
+  { path: 'locations', component: LocationsComponent },
+  { path: 'people', component: PeopleComponent },
+  { path: 'species', component: SpeciesComponent },
+  { path: 'vehicles', component: VehiclesComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'home' }
+];
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot( routes )
+  ],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule { }
