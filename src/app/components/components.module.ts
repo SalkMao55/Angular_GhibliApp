dcbb80f78775dee6ab2c8c217ae6c2af5438db11
Salomon <13030564@itcelaya.edu.mx>
2021-11-11 12:52:00 -0600

    Components: Update - We add RouterModule and RouterLink in NavBar

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
+import { RouterModule } from "@angular/router";
 import { SlideshowComponent } from './slideshow/slideshow.component';
 
 
@@ -11,7 +12,8 @@ import { SlideshowComponent } from './slideshow/slideshow.component';
     SlideshowComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports: [
     NavbarComponent,
